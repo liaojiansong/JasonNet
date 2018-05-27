e368cf2acc19aaeb18317b3dcfb4ae1266458e1c
小松果 <729631422@qq.com>
2018-05-27 21:52:58 +0800

    完成邮件报警功能

--- public/mqtt/AuthPublish.php
+++ public/mqtt/AuthPublish.php
@@ -30,11 +30,11 @@ class AuthPublish extends Base
                 'response_topic' => 'response_1',
             ];
             $mid = $this->mqtt->publish($pub_topic, json_encode($payload), 1, 0);
-            echo "当前发送鉴权信息 ID: {$mid}\n";
-            echo '-----------------------发送的鉴权消息为-------------------------------------';
-            echo "\n";
-            var_dump($payload);
-            echo "\n";
+//            echo "当前发送鉴权信息 ID: {$mid}\n";
+//            echo '-----------------------发送的鉴权消息为-------------------------------------';
+//            echo "\n";
+//            var_dump($payload);
+//            echo "\n";
 
             $this->mqtt->loop();
             unset($product_id);
--- public/mqtt/Report.php
+++ public/mqtt/Report.php
@@ -6,23 +6,26 @@
  */
 require_once('Base.php');
 
-//use PHPMailer\PHPMailer\PHPMailer;
-//use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
 
-//require '../../vendor/autoload.php';
-//
-//require '../../vendor/phpmailer/phpmailer/src/Exception.php';
-//require '../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-//require '../../vendor/phpmailer/phpmailer/src/SMTP.php';
+require '../../vendor/autoload.php';
+require '../../vendor/phpmailer/phpmailer/src/Exception.php';
+require '../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../../vendor/phpmailer/phpmailer/src/SMTP.php';
+
 class Report extends Base
 {
+    const TOKEN = 'taukoqrjhgexbdhh';
+    const MY_EMAIL = "729631422@qq.com";
+
     /**
      * 发送邮件
      * @param $target_info
      */
     public function sendEmail($target_info)
     {
-        $subject = "您的ID为{$target_info['device_id']}的设备的{$target_info['trigger_name']}被触发，详情请打开邮件";
+        $subject = "您的ID为：{$target_info['device_id']}的设备的{$target_info['trigger_name']}被触发，详情请打开邮件";
         $body = "<h3>触发器信息</h3>
                 <br/>
                 <p>触发器ID：{$target_info['trigger_id']}</p>
@@ -31,15 +34,21 @@ class Report extends Base
                 <p>触发条件：{$target_info['target_value']}</p>
                 <br/>
                 <br/>
-                <h3>触发信息</h3>
+                <h3>触发详情</h3>
                 <br/>
                 <p>设备ID：{$target_info['device_id']}</p>
-                <p>触发的值：{$target_info['send_value']}</p>
+                <p>触发的值：<span style='color: rgba(218,16,10,0.85)'>{$target_info['send_value']}</span></p>
                 <p>触发时间：{$target_info['target_time']}</p>";
         // 写进报警日志
-        $file = fopen('report.log', 'a+');
-        $flag = fwrite($file, $subject . "\n\n\n" . $body . "\n\n\n");
-        fclose($file);
+//        $file = fopen('report.log', 'a+');
+//        $flag = fwrite($file, $subject . "\n\n\n" . $body . "\n\n\n");
+//        fclose($file);
+        $email = $target_info['email'] ?? null;
+        if ($email != null) {
+            $flag = self::send($email, $subject, $body);
+        }else{
+            $flag = false;
+        }
         return $flag;
     }
 
@@ -63,7 +72,7 @@ class Report extends Base
 
                 if ($one_target_name != null) {
                     if ($this->redis->exists($one_target_name))
-                    $target_info = $this->redis->hGetAll($one_target_name);
+                        $target_info = $this->redis->hGetAll($one_target_name);
                     // TODO 返回标志位，是否报警成功 不成功再压回去
                     $flag = $this->sendEmail($target_info);
                     if ($flag) {
@@ -86,8 +95,63 @@ class Report extends Base
         }
 
     }
+
+    /**
+     * 发送邮件
+     * @param $address
+     * 收件人地址
+     * @param $nickname
+     * 收件人昵称
+     * @param $subject
+     * 邮件标题
+     * @param $body
+     * 邮件内容
+     * @return bool
+     * 成功标志
+     * @throws Exception
+     */
+    public static function send($address, $subject, $body,$nickname = '尊敬的用户')
+    {
+        $mail = new PHPMailer(); //实例化
+        $mail->IsSMTP(); // 启用SMTP
+        $mail->SMTPDebug = 0;
+        $mail->SMTPAuth = true;  //启用SMTP认证
+        // 链接qq域名邮箱的服务器地址
+        $mail->Host = 'smtp.qq.com';
+        // 设置使用ssl加密方式登录鉴权
+        $mail->SMTPSecure = 'ssl';
+        // 设置ssl连接smtp服务器的远程服务器端口号
+        $mail->Port = 465;
+
+
+        $mail->CharSet = "UTF-8"; //字符集
+        $mail->Encoding = "base64"; //编码方式
+
+
+        $mail->Username = self::MY_EMAIL;  //你的邮箱 163的
+        $mail->Password = self::TOKEN;  //你的密码
+        $mail->Subject = $subject; //邮件标题 任意
+
+        $mail->From = self::MY_EMAIL;  //发件人地址（也就是你的邮箱）
+        $mail->FromName = "JasonNet";  //发件人姓名 任意
+
+
+        $mail->AddAddress($address, $nickname);//添加收件人（地址，昵称）
+
+        $mail->IsHTML(true); //支持html格式内容
+        $mail->Body = $body; //邮件主体内容
+
+        //发送
+        if (!$mail->Send()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
 
 $obj = new Report();
 $obj->loopReport();
+//Report::send('liao_jian_song@163.com', '手机高温报警', '小松果，你的小米8手机温度过高');
 
